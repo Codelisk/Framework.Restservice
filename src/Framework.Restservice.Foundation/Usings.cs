ce6c7fb1b8817bb811f768f1a614e8abb8e355fb
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-02-03 16:01:01 +0100

    Testing

--- src/Framework.Restservice.Foundation/Usings.cs
+++ src/Framework.Restservice.Foundation/Usings.cs
@@ -3,3 +3,4 @@ global using System.Text.Json.Serialization;
 global using System.ComponentModel.DataAnnotations;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.AspNetCore.Identity;
+global using Codelisk.GeneratorAttributes;

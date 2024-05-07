7f467fcd0e9c6f67bb4f6f4db7c3ca01f7eb70a6
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-05-07 12:15:54 +0200

    Collapse swagger

--- src/Framework.Restservice.Server/ProgramHelper.cs
+++ src/Framework.Restservice.Server/ProgramHelper.cs
@@ -1,22 +1,25 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.OpenApi.Models;
 using System;
 using System.IO;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
-using Framework.Restservice.Server.Extensions;
 using AutoMapper;
+using Framework.Restservice.Server.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Framework.Restservice.Server
 {
     public static class ProgramHelper
     {
-
-        public static void ConfigureAllServices<TProfile, TDbContext>(this IServiceCollection services) where TDbContext : DbContext where TProfile : Profile, new() 
+        public static void ConfigureAllServices<TProfile, TDbContext>(
+            this IServiceCollection services
+        )
+            where TDbContext : DbContext
+            where TProfile : Profile, new()
         {
             services.AddAutoMapper<TProfile>();
             services.AddCors();
@@ -30,60 +33,76 @@ namespace Framework.Restservice.Server
         public static void ConfigureAndStartApp(this WebApplication app)
         {
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(o =>
+            {
+                o.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+            });
 
             app.AddIdentity();
             app.UseHttpsRedirection();
 
-
             app.MapControllers();
 
-            app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().SetIsOriginAllowedToAllowWildcardSubdomains());
+            app.UseCors(builder =>
+                builder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin()
+                    .SetIsOriginAllowedToAllowWildcardSubdomains()
+            );
 
             app.Run();
         }
 
-        private static void AddAutoMapper<TProfile>(this IServiceCollection services) where TProfile : Profile, new()
+        private static void AddAutoMapper<TProfile>(this IServiceCollection services)
+            where TProfile : Profile, new()
         {
-            var mapperConfig = new MapperConfiguration(
-                cfg =>
-                {
-                    cfg.AllowNullCollections = true;
-                    cfg.AddProfile<TProfile>();
-                });
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AllowNullCollections = true;
+                cfg.AddProfile<TProfile>();
+            });
 
             var mapper = mapperConfig.CreateMapper();
             services.AddSingleton(mapper);
         }
+
         private static void AddSwagger(this IServiceCollection services)
         {
             // SwaggerGen setup
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen(opt => {
-                opt.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
-                {
-                    Name = "Authorization",
-                    Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
-{
-    {
-        new OpenApiSecurityScheme
-        {
-            Name = "Bearer",
-            In = ParameterLocation.Header,
-            Reference = new OpenApiReference
+            services.AddSwaggerGen(opt =>
             {
-                Id = "Bearer",
-                Type = ReferenceType.SecurityScheme
-            }
-        },
-        new List<string>()
-    }
-});
+                opt.AddSecurityDefinition(
+                    name: "Bearer",
+                    securityScheme: new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        Description =
+                            "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer"
+                    }
+                );
+                opt.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Name = "Bearer",
+                                In = ParameterLocation.Header,
+                                Reference = new OpenApiReference
+                                {
+                                    Id = "Bearer",
+                                    Type = ReferenceType.SecurityScheme
+                                }
+                            },
+                            new List<string>()
+                        }
+                    }
+                );
             });
         }
     }
